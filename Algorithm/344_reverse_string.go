0d9c5f6dd51cd61c8bbc4b31dd732a3b58ef65a0
TianYi <leety589589@gmail.com>
2023-02-04 12:22:43 +0800

    golang day 3

---
+++ Algorithm/344_reverse_string.go
@@ -0,0 +1,11 @@
+package main
+
+//func reverseString(s []byte) {
+//	var l = len(s)
+//	var des = l / 2
+//	for i := 0; i < des; i++ {
+//		var tmp = s[i]
+//		s[i] = s[l-i-1]
+//		s[l-i-1] = tmp
+//	}
+//}
